c787975dcc8958e91c24e6a24fec651af6c82314
Bsoft Limited <63307009+BsoftLimited@users.noreply.github.com>
2023-02-10 19:48:40 +0100

    Update AppDelegate.swift
    added API key to the IOS build

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyAj8s5HSB-D5pnmtvr1Eb_bwsn6OEJL3JQ")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
